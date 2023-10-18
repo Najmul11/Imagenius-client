09c329bfdcb7511cf1097f73c348a5ad0c8443e8
Najmul11 <najmulbinnurul@gmail.com>
2023-10-19 02:58:13 +0600

    [done] private eoute fixed

--- src/router/AdminRoutes.tsx
+++ src/router/AdminRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useAppSelector } from "../redux/hook";
 import { ReactNode } from "react";
+import { useGetProfileQuery } from "../redux/api/apiSlice";
 
 type IProps = {
   children: ReactNode;
@@ -8,7 +9,10 @@ type IProps = {
 
 const AdminRoutes = ({ children }: IProps) => {
   const { user } = useAppSelector((state) => state.user);
+  const { isLoading } = useGetProfileQuery(undefined);
+
   const location = useLocation();
+  if (isLoading) return <></>;
 
   if (user && (user.role === "super admin" || user.role === "admin")) {
     return children;
--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -1,6 +1,7 @@
 import { Navigate, useLocation } from "react-router-dom";
 import { useAppSelector } from "../redux/hook";
 import { ReactNode } from "react";
+import { useGetProfileQuery } from "../redux/api/apiSlice";
 
 type IProps = {
   children: ReactNode;
@@ -8,9 +9,13 @@ type IProps = {
 
 const PrivateRoute = ({ children }: IProps) => {
   const { user } = useAppSelector((state) => state.user);
+  const { isLoading } = useGetProfileQuery(undefined);
   const location = useLocation();
 
-  if (user) {
+  if (!user || isLoading) {
+    return <></>;
+  }
+  if (user && !isLoading) {
     return children;
   }
   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
